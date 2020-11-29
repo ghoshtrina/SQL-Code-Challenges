1bc69b4ebfcdd95d40210515acbdf0626dc33417
Trina Ghosh <51182308+ghosh-trina@users.noreply.github.com>
2020-11-29 01:25:57 -0600

    Query 1

---
+++ Queries.sql
@@ -0,0 +1,6 @@
+--- 1. Your challenge is to provide a list of the customers from the customers table in the restaurant
+---  	database, which shows the customer's first name, last name, and e-mail address. 
+--- 	And it should be sorted alphabetically by last name.
+SELECT FirstName, LastName, Email FROM Customers ORDER BY LastName;
+
+--- 2. 

3c47ec6a3b4391c3f5e203f4cc8808aec54ceb9d
Trina Ghosh <51182308+ghosh-trina@users.noreply.github.com>
2020-12-03 16:02:54 -0600

    Part 1
    Completed Part 1 for restaurant.db.

--- Queries.sql
+++ Queries.sql
@@ -5,7 +5,7 @@ SELECT FirstName, LastName, Email FROM Customers ORDER BY LastName;
 
 --- 2. Create a basic table where we can record a customer's ID and the number of people they'll 
 ---	have in their party.
-DROP TABLE AnniversaryAttendees;
+---DROP TABLE AnniversaryAttendees;
 CREATE TABLE AnniversaryAttendees (
 	"CustomerID"  INTEGER,
 	"PartySize" INTEGER
@@ -41,4 +41,40 @@ VALUES (
 (SELECT CustomerID FROM Customers WHERE Email = 'atapley2j@kinetecoinc.com') 
 , 4);
 
---- 9. 
+--- 9. Lookup a reservation using Last name Stevenson on April 8th 2020.
+--- 	Also consider non-exact matches.
+SELECT * FROM Customers C INNER JOIN Reservations R ON C.CustomerID = R.CustomerID
+WHERE C.LastName LIKE "Ste%" AND R.Date LIKE "2020-04-08 %";
+
+--- 10. Take a reservation. 
+SELECT * FROM Customers WHERE Email = 'smac@rouxacademy.com';
+INSERT INTO Customers ('FirstName', 'LastName', 'Email', 'Phone')
+VALUES ('Sam', 'McAdams', 'smac@rouxacademy.com', '(555) 555-1212');
+INSERT INTO Reservations (CustomerID, PartySize, Date)
+VALUES 
+( (SELECT CustomerID FROM Customers WHERE Email='smac@rouxacademy.com'),
+	5, "2020-07-14 18:00:000");
+SELECT * FROM Reservations WHERE Date = '2020-07-14 18:00:000';
+
+--- 11. Take a delivery order.
+INSERT INTO Orders (CustomerID, OrderDate) VALUES
+((SELECT CustomerID FROM Customers WHERE FirstName='Loretta' AND LastName='Hundey'),
+"2020-03-20 14:00:000");
+SELECT OrderID FROM Orders WHERE CustomerID = 70 AND OrderDate = "2020-03-20 14:00:000";
+INSERT INTO OrdersDishes (OrderID, DishID) VALUES
+(1002, (SELECT DishID FROM Dishes WHERE name = "House Salad")),
+(1002, (SELECT DishID FROM Dishes WHERE name = "Mini Cheeseburgers")),
+(1002, (SELECT DishID FROM Dishes WHERE name = "Tropical Blue Smoothie"));
+SELECT SUM(D.Price) FROM Dishes D INNER JOIN OrdersDishes O ON D.DishID=O.DishID
+WHERE O.OrderID=1002;
+
+--- 12. Track your customer's favorite dishes.
+UPDATE Customers SET
+FavoriteDish = (SELECT DishID FROM Dishes WHERE name="Quinoa Salmon Salad")
+WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE FirstName="Cleo" AND LastName="Goldwater");
+
+--- 13. Top 5 customers.
+SELECT C.CustomerID, C.FirstName, C.LastName, C.Email, COUNT(O.OrderID) 
+FROM Customers C INNER JOIN Orders O ON C.CustomerID=O.CustomerID
+GROUP BY C.CustomerID, C.FirstName, C.LastName, C.Email
+ORDER BY COUNT(O.OrderID) DESC LIMIT 5;
